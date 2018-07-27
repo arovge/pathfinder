242183e693b36d65a5527c495eb396ea1d19d3b2
Austin Rovge <rovgea@msoe.edu>
2018-07-26 22:01:07 -0500

    added basic javafx files

---
+++ src/rovgea/Pathfinder.java
@@ -0,0 +1,66 @@
+/*
+ * Project: Pathfinder
+ * Author: Austin Rovge
+ * Date: 3/26/2018
+ */
+
+package rovgea;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+
+/**
+ * This is the driver class for Lab04. It starts the JavaFX window and sets up the logger.
+ */
+public class Pathfinder extends Application {
+
+    /** This is the logger object used by all classes in the program */
+    public static Logger logger;
+
+    /**
+     * This is the start method for the application.
+     * The information about the window is created here and so is information about the logger.
+     * @param primaryStage Stage object that is created based on the FXML file
+     * @throws Exception generic Exception object if an error is occurred.
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        final int stageWidth = 600;
+        final int stageHeight = 500;
+
+        Parent root = FXMLLoader.load(getClass().getResource("Pathfinder.fxml"));
+        primaryStage.setTitle("Pathfinder");
+        primaryStage.setScene(new Scene(root, stageWidth, stageHeight));
+        primaryStage.setResizable(false);
+
+        /*
+         * Create logger. Add handler. Remove parent handler.
+         */
+        Pathfinder.logger = Logger.getLogger("Pathfinder Logger");
+        try {
+            FileHandler handler = new FileHandler(System.getProperty("user.dir")
+                    + File.separator + "log.txt");
+            Pathfinder.logger.addHandler(handler);
+            Pathfinder.logger.setUseParentHandlers(false);
+        } catch (IOException e) {
+            Pathfinder.logger.severe("Could not add handler to logger");
+        }
+
+        primaryStage.show();
+    }
+
+    /**
+     * This is the main method for the program. It starts the FX application.
+     * @param args String array passed into launch()
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/rovgea/PathfinderController.java
@@ -0,0 +1,46 @@
+/*
+ * Project: Pathfinder
+ * Author: Austin Rovge
+ * Date: 3/26/2018
+ */
+
+package rovgea;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+/**
+ * This is the controller for the Pathfinder JavaFX file.
+ * It holds all of the methods found in the Pathfinder.fxml file.
+ */
+public class PathfinderController {
+
+    /** Label object used for storing the time taken to run the path finding algorithm. */
+    @FXML
+    private Label timeLabel;
+
+    /** Label object used for displaying the current mode. */
+    @FXML
+    private Label modeLabel;
+
+    /**
+     * This method runs the current algorithm against the map.
+     */
+    public void run() {
+
+    }
+
+    /**
+     * This method shows the intermediate steps the algorithm goes through to find the correct path.
+     */
+    public void showSteps() {
+
+    }
+
+    /**
+     * This method enables diagonal tiles to be drawn on the map.
+     */
+    public void enableDiagonal() {
+
+    }
+}
