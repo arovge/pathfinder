b4514a506ef89eaa5eb9b034d11168a7b50857af
Austin Rovge <rovgea@msoe.edu>
2018-07-28 20:58:52 -0500

    added drag over toggle functionality

--- src/rovgea/MapRectangle.java
+++ src/rovgea/MapRectangle.java
@@ -5,19 +5,33 @@ import javafx.scene.shape.Rectangle;
 
 public class MapRectangle extends Rectangle {
 
+    public final static Paint activeColor = Paint.valueOf("#b8b7b7");
+    public final static Paint inActiveColor = Paint.valueOf("#e0e0e0");
+
     private boolean isActive;
 
     public MapRectangle(double x, double y, double width, double height) {
         super(x, y, width, height);
         this.isActive = false;
+        this.setFill(MapRectangle.inActiveColor);
     }
 
     public boolean isActive() {
         return this.isActive;
     }
 
-    public void toggleActive() {
-        this.isActive = !this.isActive;
-        this.setFill(Paint.valueOf("#b8b7b7"));
+    public void toggleActive(boolean bool) {
+        if (this.isActive != bool) {
+            this.isActive = !this.isActive;
+            this.setColor();
+        }
+    }
+
+    private void setColor() {
+        if (this.isActive) {
+            this.setFill(MapRectangle.activeColor);
+        } else {
+            this.setFill(MapRectangle.inActiveColor);
+        }
     }
 }
--- src/rovgea/PathfinderController.java
+++ src/rovgea/PathfinderController.java
@@ -8,10 +8,9 @@ package rovgea;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Paint;
-import javafx.scene.shape.Rectangle;
 
 /**
  * This is the controller for the Pathfinder JavaFX file.
@@ -21,7 +20,7 @@ public class PathfinderController {
 
     private final int x = 19;
     private final int y = 15;
-    private Rectangle[][] rectangles = new Rectangle[x][y];
+    private MapRectangle[][] rectangles = new MapRectangle[x][y];
 
     /** Label object used for storing the time taken to run the path finding algorithm. */
     @FXML
@@ -39,25 +38,43 @@ public class PathfinderController {
         final double width = 31.0;
         final double height = 31.0;
 
+        this.pane.setOnMouseDragged(e -> {
+            final int paneX = (int) (e.getX() / width);
+            final int paneY = (int) (e.getY() / height);
+
+            // prevents an ArrayIndexOutOfBoundsException and is probably cheaper resource wise than try/catch
+            if (0 <= paneX && paneX < this.x && 0 <= paneY && paneY < this.y) {
+                MapRectangle mapRect = this.rectangles[paneX][paneY];
+
+                if (e.getButton() == MouseButton.PRIMARY) {
+                    mapRect.toggleActive(true);
+                } else if (e.getButton() == MouseButton.SECONDARY) {
+                    mapRect.toggleActive(false);
+                }
+            }
+        });
+
         for (int i = 0; i < this.x; i++) {
             for (int j = 0; j < this.y; j++) {
 
                 MapRectangle mapRect = new MapRectangle(i * width, j * height, width, height);
 
-                mapRect.setFill(Paint.valueOf("#e0e0e0"));
-
                 mapRect.addEventFilter(MouseEvent.MOUSE_ENTERED, e -> {
-                    mapRect.setFill(Paint.valueOf("#b8b7b7"));
+                    mapRect.setFill(MapRectangle.activeColor);
                 });
 
                 mapRect.addEventFilter(MouseEvent.MOUSE_EXITED, e -> {
                     if (!mapRect.isActive()) {
-                        mapRect.setFill(Paint.valueOf("#e0e0e0"));
+                        mapRect.setFill(MapRectangle.inActiveColor);
                     }
                 });
 
                 mapRect.addEventFilter(MouseEvent.MOUSE_CLICKED, e -> {
-                    mapRect.toggleActive();
+                    if (e.getButton() == MouseButton.PRIMARY) {
+                        mapRect.toggleActive(true);
+                    } else if (e.getButton() == MouseButton.SECONDARY) {
+                        mapRect.toggleActive(false);
+                    }
                 });
 
                 this.pane.getChildren().add(mapRect);
